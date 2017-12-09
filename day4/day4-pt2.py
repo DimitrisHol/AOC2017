af66a1f90b27e2455cb8940791749614f951b1c7
DimitrisHol <dimitris.hol11@gmail.com>
2017-12-09 18:18:39 +0200

    Day4 complete!

---
+++ day4/day4-pt2.py
@@ -0,0 +1,13 @@
+handle = open("input.txt")
+count = 0
+for line in handle:
+    tempList = line.rstrip().split()
+    for index in range(len(tempList)):
+        tempList[index] = "".join(sorted(tempList[index]))
+    print(tempList)
+    for word in tempList:
+        if (tempList.count(word) > 1):
+            count -=1
+            break;
+    count +=1
+print(count)
---
+++ day4/test.py
@@ -0,0 +1,5 @@
+x = ["abc" , "cba"]
+print(x)
+for index in range(len(x)):
+    x[index] = "5"
+print(x)
