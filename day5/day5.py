304c98a53fc579b934d49dc6b095795fb6601186
DimitrisHol <dimitris.hol11@gmail.com>
2017-12-15 18:27:47 +0200

    Day 7 completed!

--- day5/day5.py
+++ day5/day5.py
@@ -11,10 +11,17 @@ index = 0
 steps = 0
 
 while (index <len(lst)):
+
+    #-----PART 1 -----#
+    #lst[index] +=1
+    #-----PART 1 -----#
+
+    #-----PART 2 -----#
     if (lst[index] > 3):
         lst[index] -=1
     else:
         lst[index] +=1
+    #-----PART 2 -----#
 
     index = index + lst[index] -1
     steps +=1
