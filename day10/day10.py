6fab66136981bf90ec9db44065cd3d280eb1ae4b
DimitrisHol <dimitris.holl11@gmail.com>
2018-01-04 23:35:48 +0200

    Completed part 1

--- day10/day10.py
+++ day10/day10.py
@@ -1,33 +1,37 @@
-# NOTE: WIP
-
 handle = open("input.txt")
-
 
 x = handle.read().rstrip().split(",")
 length = list()
 for string in x:
     length.append(int(string))
 
+
+a = [x for x in range(256)]
 pos = 0
 skip = 0
 
-itList = [0,1,2,3,4]
-length = [3, 4, 1, 5]
+for l in length:
+    print("Starting with : a = " , a )
+    print("Selecting " , pos)
+    b = list()
+    steps = 0
+    while steps < l:
+        b.append(a[ (pos + steps) % len(a) ])
+        steps+=1
+        print("b =" , b)
 
+    b.reverse()
+    print("Reverse b: " , b)
 
-start = 1
-x = 2
-print ("Start : " , itList[start:start+x])
-if start == 0:
-    print("End   : " , itList[x-1::-1])
-else: # TODO: FIND A WAY TO MAKE THIS WORK 100%
-    print("End   : " , itList[:start:-1])
+    steps = 0
+    while steps < l:
+        a[(pos+steps) % len (a)] = b[steps]
+        steps+=1
 
+    pos = (pos + l + skip) % len(a)
+    skip +=1
 
+    print("Finishing with : a = " , a )
 
-# itList[:3] = itList[2::-1]
-
-
-
-
+print(a[0]* a[1])
 handle.close()
