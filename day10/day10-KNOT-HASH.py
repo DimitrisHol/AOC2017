9feab04b08c2fb8e3f514801167d76ee788e3c94
Dimitris Hol <dimitris.hol11@gmail.com>
2018-01-06 00:57:29 +0200

    Day10 Complete!
    Completed Part 2 of KnotHash!

--- day10/day10-KNOT-HASH.py
+++ day10/day10-KNOT-HASH.py
@@ -2,42 +2,64 @@ handle = open("input.txt")
 
 x = handle.read().rstrip()
 length = list()
-x = "1,2,3"
 for string in x:
-    length.append(ord(string))
+	length.append(ord(string))
 for l in [17, 31, 73, 47, 23]:
-    length.append(l)
+	length.append(l)
 
 
 a = [x for x in range(256)]
 pos = 0
 skip = 0
+
 for index in range(64):
-    for l in length:
-        print("Starting with : a = " , a )
-        print("Selecting " , pos)
-        b = list()
-        steps = 0
-        while steps < l:
-            b.append(a[ (pos + steps) % len(a) ])
-            steps+=1
-            print("b =" , b)
+	for l in length:
+		# print("Starting with : a = " , a )
+		# print("Selecting " , pos)
+		b = list()
+		steps = 0
+		while steps < l:
+			b.append(a[ (pos + steps) % len(a) ])
+			steps+=1
+			# print("b =" , b)
 
-        b.reverse()
-        print("Reverse b: " , b)
+		b.reverse()
+		# print("Reverse b: " , b)
 
-        steps = 0
-        while steps < l:
-            a[(pos+steps) % len (a)] = b[steps]
-            steps+=1
+		steps = 0
+		while steps < l:
+			a[(pos+steps) % len (a)] = b[steps]
+			steps+=1
 
-        pos = (pos + l + skip) % len(a)
-        skip +=1
+		pos = (pos + l + skip) % len(a)
+		skip +=1
 
-        print("Finishing with : a = " , a )
+		# print("Finishing with : a = " , a )
+# print(a)
 
-# TODO: EVERY 16 NUMBERS : XOR!!
+denseHash = list()
 
-# TODO: THE REST
 
+for index in range(16):
+	denseHashObject = a[16 * index]
+	innerIndex = 1
+	while innerIndex < 16:
+		denseHashObject = denseHashObject ^ a[16 * index + innerIndex]
+		innerIndex +=1
+
+	denseHash.append(denseHashObject)
+
+print("\n\n--------------HEX!--------------")
+
+hexHash = list()
+
+for number in denseHash:
+	hexHash.append(hex(number))
+
+result = list()
+for string in hexHash:
+	result.append(string[2:])
+
+print("".join(result))
+print("--------------HEX!--------------\n\n")
 handle.close()
